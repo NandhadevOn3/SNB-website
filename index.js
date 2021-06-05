6e8b40bc56f4209a0f881c518bb609ba6a6e5617
NandhadevOn3 <nandhabalan3998@gmail.com>
2021-06-06 00:58:35 +0530

    modify index.js

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'static/1.Home.html'));
 })
  
-var server = app.listen(5000)
+var server = app.listen(process.env.PORT || 5000)
