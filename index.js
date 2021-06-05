d857ef87e592c842fafcf415f9bf79f01eafaa14
NandhadevOn3 <nandhabalan3998@gmail.com>
2021-06-06 00:25:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+const path = require('path');
+
+app.use('/static', express.static(path.join(__dirname, 'static')))
+app.use(express.static('static'))
+ 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'static/1.Home.html'));
+})
+ 
+var server = app.listen(5000)
